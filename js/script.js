a9774a3ff9b713b7fb3b38ea24fa293c71e1cc68
Joanna Buchała <joanna.b2@op.pl>
2018-12-10 22:27:10 +0100

    delete task function, finishing touches

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
+document.getElementById('addTask').addEventListener('click', function() {
+	toDoList();
+});
 
 function toDoList() {
 	var item = document.getElementById('todo-input').value;
 	var text = document.createTextNode(item);
 	var newItem = document.createElement('li');
 	newItem.appendChild(text);
-	document.getElementById('toDoList').appendChild(newItem);
+	newItem.onclick = removeItem;
+
+	if (item === '') {
+		alert('You need to write something!');
+	} else {
+		document.getElementById('toDoList').appendChild(newItem);
+	}
 	document.getElementById('todo-input').value = '';
 }
 
+function removeItem(e) {
+	e.target.parentElement.removeChild(e.target);
+}
